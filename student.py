19ec1306dd9d42e2fe98bd78d0cae72c05f689e8
EdgarasSp <dev.applications.esp@gmail.com>
2022-06-06 20:26:13 +0000

    updated to fancy classmethod

--- student.py
+++ student.py
@@ -1,7 +1,10 @@
+
 from datetime import date, timedelta
+
 
 class Student:
     """ A Student class as a basis for method testing """
+
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
@@ -12,3 +15,10 @@ class Student:
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
--- test_students.py
+++ test_students.py
@@ -1,16 +1,54 @@
 import unittest
 from student import Student
 
+# ESP BASIC ORIGINAL METHOD
+
+# class TestStudent(unittest.TestCase):
+#    def test_full_name(self):
+#        student = Student("John", "Doe")
+#        self.assertEqual(student.full_name, "John Doe")
+#
+#    def test_alert_santa(self):
+#        student = Student("John",  "Doe")
+#        student.alert_santa()
+#        self.assertTrue(student.naughty_list)
+#
+#    def test_email(self):
+#        student = Student("John",  "Doe")
+#        self.assertEqual(student.email, "john.doe@email.com")
+#
+# if __name__ == "__main__":
+#    unittest.main()
 
 class TestStudent(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
+
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student("John",  "Doe")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
 if __name__ == "__main__":
     unittest.main()
